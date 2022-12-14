173d2f11599c7350e4ba023e94eedc2adad9daa6
Tomas Fidransky <tom.fidransky@gmail.com>
2022-12-14 14:47:30 +0100

    zkouška commitu bez dodatečných složek z předchozích úkolů

---
+++ Praha_kontejnery.py
@@ -0,0 +1,9 @@
+import json
+from geopy.distance import geodesic
+
+# načteme vstupní GeoJSON soubory
+with open('adresy.geojson') as f:
+    adresy = json.load(f)
+with open('kontejnery.geojson') as f:
+    kontejnery = json.load(f)
+    

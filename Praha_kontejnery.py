4da1bea3228a07c786182b8b5350c0d27af763f9
Tomas Fidransky <tom.fidransky@gmail.com>
2022-12-27 13:45:50 +0100

    Přidání výpočtu vzdálenosti, mediánu, nul. vzdál.

--- Praha_kontejnery.py
+++ Praha_kontejnery.py
@@ -1,5 +1,6 @@
 import json
 import pyproj
+import math
 
 def open_geojson():
     with open('adresy.geojson', encoding = 'utf-8') as f:
@@ -11,53 +12,70 @@ def open_geojson():
 # potřeba převést adresy na S-JTSK z WGS-84
 
 def calculate_distance(adresa, container):
-    # udělat výpočet vzdálenosti
-    pass
+    # převod souřadnic adresy na S-JTSK
+    wgs84 = pyproj.CRS("EPSG:4326")
+    sjtsk = pyproj.CRS("EPSG:5514")
+    transformer = pyproj.Transformer.from_crs(wgs84, sjtsk)
+    adresa_sjtsk = transformer.transform(adresa["geometry"]["coordinates"][0], adresa["geometry"]["coordinates"][1])
+
+    # výpočet vzdálenosti pomocí Pythagorovy věty
+    x1, y1 = adresa_sjtsk
+    x2, y2 = container["geometry"]["coordinates"]
+    distance = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+    return distance
 
 def nearest_container(adresy, kontejnery):
-    total_distance = 0
+    distances = []
     max_distance = 0
     farthest_address = ""
-    count = 0 # slouží pro průměrnou vzdálenost, problém, že se nepřičítá hodnota
-
     for adresa in adresy:
         if isinstance(adresa, dict):
-            street = adresa["addr:street"]
-            housenumber = adresa["addr:housenumber"]
+            street = adresa["properties"]["addr:street"]
+            housenumber = adresa["properties"]["addr:housenumber"]
             nearest_container = None
             nearest_distance = None
 
             for container in kontejnery:
-                name = container["STATIONNAME"]
-                access = container["PRISTUP"]
-                if access == "volně":
+                name = container["properties"]["STATIONNAME"]
+                access = container["properties"]["PRISTUP"]
+                if access == "obyvatelům domu":
+                    nearest_distance = 0
+                elif access == "volně":
                     distance = calculate_distance(adresa, container)
                     if nearest_distance is None or distance < nearest_distance:
-                        nearest_container = container
+                        nearest_container = container["properties"]
                         nearest_distance = distance
 
-                    if nearest_distance is None or distance < nearest_distance:
-                        nearest_container = container
-                        nearest_distance = distance
-
-            if nearest_distance is not None:
-                total_distance += nearest_distance
-                count += 1
+            if nearest_distance is not None and nearest_distance > 0:
+                distances.append(nearest_distance)
 
                 if nearest_distance > max_distance:
                     max_distance = nearest_distance
                     farthest_address = f"{street} {housenumber}"
 
-    avg_distance = total_distance / count
+            if max_distance > 10000:
+                    print("Některá adresa je vzdálenější než 10 km.")
 
-    print(f"Průměrná vzdálenost k veřejnému kontejneru je {avg_distance:.0f} metrů")
-    print(f"Nejvzdálenější adresa je {farthest_address} a vzdálenost k nejbližšímu kontejneru je {max_distance:.0f} metrů")
+    print(len(distances))
 
-    if max_distance > 10000:
-        raise ValueError("Některá adresa je vzdálenější než 10 km od nejbližšího kontejneru")
+    avg_distance = sum(distances) / len(distances)
+    distances.sort()
+    if len(distances) % 2 == 0:
+        median_distance = (distances[len(distances) // 2] + distances[len(distances) // 2 - 1]) / 2
+    else:
+        median_distance = distances[len(distances) // 2]
+
+    print(f"Průměrná vzdálenost od adres k veřejným kontejnerům: {avg_distance:.2f} metrů")
+    print(f"Mediánová vzdálenost od adres k veřejným kontejnerům: {median_distance:.2f} metrů")
+    print(f"Nejvzdálenější adresa od nejbližšího veřejného kontejneru je: {farthest_address}, vzdálenost je {max_distance:.2f} metrů")
+
+
+
+
 
 adresy, kontejnery = open_geojson()
-nearest_container(adresy, kontejnery)
+nearest_container(adresy["features"], kontejnery["features"])
 
 
 #Program by měl vypsat průměrnou vzdálenost k veřejnému kontejneru a ze které adresy je to k nejbližšímu kontejneru nejdále a jak to je daleko (v metrech, zaokrouhleno na celé metry).
